c2362212aaf6473e4a8f1d2642e979c7c6432c4a
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2025-08-16 22:03:21 +0300

    check commit

--- infra/pm2/ecosystem.config.cjs
+++ infra/pm2/ecosystem.config.cjs
@@ -12,10 +12,10 @@ module.exports = {
     },
     {
       name: "medusa-api-test",
-      cwd: "/home/deploy/apps/medusa-paperfox-test/current",
+      cwd: "/home/deploy/apps/medusa-paperfox/current",
       script: "npm",
       args: "start",
-      env: { NODE_ENV: "production", PORT: 9001 },
+      env: { NODE_ENV: "production", PORT: 9000 },
       autorestart: true,
       max_memory_restart: "512M",
       time: true,
@@ -39,7 +39,7 @@ module.exports = {
         "npm run build",
         "ln -sf /home/deploy/apps/medusa-paperfox/shared/.env /home/deploy/apps/medusa-paperfox/current/.env",
         "pm2 startOrReload /home/deploy/apps/medusa-paperfox/current/infra/pm2/ecosystem.config.cjs --only medusa-api --env production",
-        "pm2 save"
+        "pm2 save",
       ].join(" && "),
       env: { NODE_ENV: "production", PORT: 9000 },
     },
@@ -48,69 +48,21 @@ module.exports = {
     test: {
       user: "deploy",
       host: "paperfox",
-      ref: "origin/deploy/test",              // <- інша гілка
+      ref: "origin/deploy/test", // <- інша гілка
       repo: "git@github.com:VolodymyrVlasov/PaperfoxMedusa.git",
-      path: "/home/deploy/apps/medusa-paperfox-test", // <- інша директорія
+      path: "/home/deploy/apps/medusa-paperfox",
       "pre-deploy-local": "",
       "post-deploy": [
         'export NVM_DIR="$HOME/.nvm"',
         '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" || true',
-        "cd /home/deploy/apps/medusa-paperfox-test/current",
+        "cd /home/deploy/apps/medusa-paperfox/current",
         "npm ci",
         "npm run build",
-        "ln -sf /home/deploy/apps/medusa-paperfox-test/shared/.env /home/deploy/apps/medusa-paperfox-test/current/.env",
-        "pm2 startOrReload /home/deploy/apps/medusa-paperfox-test/current/infra/pm2/ecosystem.config.cjs --only medusa-api-test --env production",
-        "pm2 save"
+        "ln -sf /home/deploy/apps/medusa-paperfox/shared/.env /home/deploy/apps/medusa-paperfox/current/.env",
+        "pm2 startOrReload /home/deploy/apps/medusa-paperfox/current/infra/pm2/ecosystem.config.cjs --only medusa-api --env production",
+        "pm2 save",
       ].join(" && "),
-      env: { NODE_ENV: "production", PORT: 9001 },
+      env: { NODE_ENV: "production", PORT: 9000 },
     },
   },
 };
-
-// module.exports = {
-//   apps: [
-//     {
-//       name: "medusa-api",
-//       cwd: "/home/deploy/apps/medusa-paperfox/current",
-//       script: "npm",
-//       args: "start",
-//       env: {
-//         NODE_ENV: "production",
-//         PORT: 9000,
-//       },
-//       autorestart: true,
-//       max_memory_restart: "512M",
-//       time: true,
-//     },
-//   ],
-
-//   deploy: {
-//     production: {
-//       user: "deploy",
-//       host: "paperfox", // або IP
-//       ref: "origin/main",
-//       repo: "git@github.com:VolodymyrVlasov/PaperfoxMedusa.git",
-//       path: "/home/deploy/apps/medusa-paperfox",
-//       "pre-deploy-local": "",
-//       "post-depleoy": [
-//         // (опціонально) підвантажити nvm, якщо PM2 запускається без профілю шелла
-//         "export NVM_DIR=\"$HOME/.nvm\"",
-//         "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\" || true",
-
-//         // перейти в актуальний реліз і зібрати
-//         "cd /home/deploy/apps/medusa-paperfox/current",
-//         "npm ci",
-//         "npm run build",
-
-//         // старт/релод через абсолютний шлях до цього ж файлу
-//         "ln -sf /home/deploy/apps/medusa-paperfox/shared/.env /home/deploy/apps/medusa-paperfox/current/.env",
-//         "pm2 startOrReload /home/deploy/apps/medusa-paperfox/current/infra/pm2/ecosystem.config.cjs --only medusa-api --env production",
-//         "pm2 save"
-//       ].join(" && "),
-//       env: {
-//         NODE_ENV: "production",
-//         PORT: 9000,
-//       },
-//     },
-//   },
-// };

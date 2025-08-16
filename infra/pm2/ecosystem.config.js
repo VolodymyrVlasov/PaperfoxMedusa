a199878a8609377f95f3c359c11a5b4aa192e74a
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2025-08-16 17:01:26 +0300

    ecosystem.config.js - cjnvert to ES modules

--- infra/pm2/ecosystem.config.js
+++ infra/pm2/ecosystem.config.js
@@ -1,17 +1,15 @@
-module.exports = {
-  apps: [
-    {
-      name: "medusa-api",
-      cwd: "/home/deploy/apps/medusa-paperfox",
-      script: "npm",
-      args: "start",
-      env: {
-        NODE_ENV: "production",
-        PORT: process.env.PORT || 9000,
-      },
-      autorestart: true,
-      max_memory_restart: "512M",
-      time: true,
+export const apps = [
+  {
+    name: "medusa-api",
+    cwd: "/home/deploy/apps/medusa-paperfox",
+    script: "npm",
+    args: "start",
+    env: {
+      NODE_ENV: "production",
+      PORT: process.env.PORT || 9000,
     },
-  ],
-}
+    autorestart: true,
+    max_memory_restart: "512M",
+    time: true,
+  },
+];
